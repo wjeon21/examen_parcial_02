0e05d74e0a43ed15fc611417fcc1520f40cccfd1
wjeon21 <80170637+wjeon21@users.noreply.github.com>
2021-06-12 11:47:36 -0600

    Add files via upload

---
+++ Examen_Parcial_2/resources/js/misFunciones.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    /*Boton Toggle*/
+    $('.btnToggle').click(function(){
+        $('#miMenu').toggle(1500);
+    })
+    $('.btnToggle2').click(function(){
+        $('#miMenu2').toggle(1500);
+    })
+})
